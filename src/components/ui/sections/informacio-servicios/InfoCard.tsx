64271260fb4acfb7d24ee7676bd2b01bc7f92314
martinezdap <martinezignaciodev@gmail.com>
2025-06-14 11:40:21 -0300

    Estilos modificados en la vista auth

--- src/components/ui/sections/informacio-servicios/InfoCard.tsx
+++ src/components/ui/sections/informacio-servicios/InfoCard.tsx
@@ -11,7 +11,7 @@ export default function InfoCard({
         <div className="bg-blue-500 shadow-lg rounded-xl p-5 text-center transition hover:scale-105">
             <div className="flex justify-between items-start">
                 <div className="text-blue-200 text-2xl mb-4">{icon}</div>
-                <h3 className="text-xl font-semibold mb-2">{title}</h3>
+                <h3 className="text-gray-100 text-xl font-semibold mb-2">{title}</h3>
             </div>
             <p className="text-gray-100 text-sm">{description}</p>
         </div>
