99044d39f99055fbe813b461d859edf0d4ae97b3
Bautista Heyde <bautistaheyde@hotmail.com>
2025-06-14 23:19:40 -0300

    info: Agregue funcionalidad al User Profile y modifique el Layout de App para que en ciertas rutas no salga la navbar (ENDPOINTS DE SERVICES ACTUALIZADO)

--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -1,13 +1,11 @@
 import axios from "axios";
 import { UserProfile } from "@/types";
 
-// Obtener perfil actual
 export const getProfile = async (): Promise<UserProfile> => {
-  const res = await axios.get<UserProfile>("/api/profile");
+  const res = await axios.get<UserProfile>("/users/ById");
   return res.data;
 };
 
-// Actualizar perfil
 export const updateProfile = async (data: UserProfile): Promise<void> => {
-  await axios.put("/api/profile", data);
+  await axios.put("/users/update", data);
 };
