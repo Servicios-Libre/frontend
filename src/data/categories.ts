dc5888f940119a0275666fd11654e76573800a00
Bautista Heyde <bautistaheyde@hotmail.com>
2025-06-16 15:20:54 -0300

    info: Modifique ciertos aspectos para dejar una demo listo

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,11 +2,12 @@ import { Categoria } from "@/types/index";
 
 export const categories: Categoria[] = [
   { id: "c1f2e3d4-a5b6-4c7d-8e9f-0a1b2c3d4e5f", name: 'Agricultura', icon: 'tractor' },
+  { id: "b6c7d8e9-f0a1-4b2c-3d4e-5f6a7b8c9d0e", name: 'Fitness', icon: 'dumbbell' },
+  { id: "f7e8d9c0-b1a2-4c3d-5e6f-7a8b9c0d1e2f", name: 'Marketing digital', icon: 'bullhorn' },
   { id: "d2e3f4a5-b6c7-4d8e-9f0a-1b2c3d4e5f6a", name: 'Desarrollo web', icon: 'laptop-code' },
-  { id: "e3f4a5b6-c7d8-4e9f-0a1b-2c3d4e5f6a7b", name: 'Salud', icon: 'user-nurse' },
-  { id: "f4a5b6c7-d8e9-4f0a-1b2c-3d4e5f6a7b8c", name: 'Educación', icon: 'chalkboard-teacher' },
-  { id: "a5b6c7d8-e9f0-4a1b-2c3d-4e5f6a7b8c9d", name: 'Reparaciones', icon: 'screwdriver-wrench' },
+  { id: "e8f9a0b1-c2d3-4e5f-6a7b-8c9d0e1f2a3b", name: 'Ventas', icon: 'handshake' },
   { id: "d8e9f0a1-b2c3-4d4e-5f6a-7b8c9d0e1f2a", name: 'Estética', icon: 'face-smile' },
-  { id: "c7d8e9f0-a1b2-4c3d-4e5f-6a7b8c9d0e1f", name: 'Ventas / Locución', icon: 'bullhorn' },
-  { id: "b6c7d8e9-f0a1-4b2c-3d4e-5f6a7b8c9d0e", name: 'Fitness', icon: 'dumbbell' }
+  { id: "e3f4a5b6-c7d8-4e9f-0a1b-2c3d4e5f6a7b", name: 'Salud', icon: 'user-nurse' },
+  { id: "a5b6c7d8-e9f0-4a1b-2c3d-4e5f6a7b8c9d", name: 'Reparación', icon: 'screwdriver-wrench' },
+  { id: "c7d8e9f0-a1b2-4c3d-4e5f-6a7b8c9d0e1f", name: 'Desarrollo personal', icon: 'user-astronaut' }
 ];
