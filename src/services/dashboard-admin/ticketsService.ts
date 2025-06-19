4d93382fb1698786d513761d439c6941ea23c21e
Bautista Heyde <bautistaheyde@hotmail.com>
2025-06-19 14:50:57 -0300

    info: mando token al get de tikects

--- src/services/dashboard-admin/ticketsService.ts
+++ src/services/dashboard-admin/ticketsService.ts
@@ -4,6 +4,12 @@ import { Ticket } from "@/types";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getAllTickets = async (): Promise<Ticket[]> => {
-  const res = await axios.get(`${API_URL}/tickets`);
-  return res.data;
+    const token = localStorage.getItem("token");
+    if (!token) throw new Error("No se encontró el token");
+    const res = await axios.get(`${API_URL}/tickets`,  {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+    });
+    return res.data;
 };
