acbdaa65de776016cea910259e3e82138d957127
Martin Tavella <martutavella@gmail.com>
2025-07-01 01:40:47 -0300

    fix

--- src/app/solicitud/page.tsx
+++ src/app/solicitud/page.tsx
@@ -1,10 +1,7 @@
 import { ServiceForm } from "@/components/solicitudservices/ServiceForm";
-import { useEffect } from "react";
 
 export default function SolicitudPage() {
-  useEffect(() => {
-    document.title = "Servicio Libre - Crear servicio";
-  }, []);
+  
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
       <ServiceForm />
