d07452bc87d63130e9182cd5f6cea4616b15cda5
martin-tavella <martutavella@gmail.com>
2025-07-09 00:02:59 -0300

    LoginForm ahora es un formulario controlado, al mandar la petición en register y login se muestra un loader

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -3,6 +3,9 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { validateLoginForm } from "@/utils/validations/validationLoginForm";
 
 type Props = {
   setMessage: (msg: string) => void;
@@ -11,17 +14,40 @@ type Props = {
 export default function LoginForm({ setMessage }: Props) {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const router = useRouter();
+  const [error, setError] = useState<{ email?: string; password?: string }>({});
+  const [loadingResponse, setLoadingResponse] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
     setMessage("");
   };
 
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { name } = e.target;
+    const validationErrors = validateLoginForm(formData);
+
+    if (validationErrors[name as keyof typeof validationErrors]) {
+      setError((prev) => ({
+        ...prev,
+        [name]: validationErrors[name as keyof typeof validationErrors],
+      }));
+    } else {
+      setError((prev) => {
+        const newErrors = { ...prev };
+        delete newErrors[name as keyof typeof newErrors];
+        return newErrors;
+      });
+    }
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setMessage("");
+    const validationErrors = validateLoginForm(formData);
+    setError(validationErrors);
+    if (Object.keys(validationErrors).length > 0) return;
     try {
-
+      setLoadingResponse(true);
       const result = await signIn("credentials", {
         redirect: false,
         email: formData.email,
@@ -39,6 +65,8 @@ export default function LoginForm({ setMessage }: Props) {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
       setMessage("Error inesperado al iniciar sesión");
+    } finally {
+      setLoadingResponse(false);
     }
   };
 
@@ -51,8 +79,10 @@ export default function LoginForm({ setMessage }: Props) {
         className="px-4 py-2 border border-gray-300 rounded-md text-sm focus:ring-2 focus:ring-blue-500 focus:outline-none"
         value={formData.email}
         onChange={handleChange}
+        onBlur={handleBlur}
         required
       />
+       {error.email && <p className="text-red-500 text-xs mt-1">{error.email}</p>}
       <input
         name="password"
         type="password"
@@ -60,13 +90,22 @@ export default function LoginForm({ setMessage }: Props) {
         className="px-4 py-2 border border-gray-300 rounded-md text-sm focus:ring-2 focus:ring-blue-500 focus:outline-none"
         value={formData.password}
         onChange={handleChange}
+        onBlur={handleBlur}
         required
       />
+      {error.password && <p className="text-red-500 text-xs mt-1">{error.password}</p>}
       <button
         type="submit"
         className="bg-blue-500 hover:bg-blue-600 text-white font-bold text-sm py-2 rounded-md mt-2 transition-colors cursor-pointer"
       >
-        Iniciar Sesión
+          {loadingResponse ? (
+                  <>
+                    <FontAwesomeIcon icon={faSpinner} className="animate-spin mr-2" />
+                    Iniciando sesión...
+                  </>
+                ) : (
+                  "Iniciar sesión"
+                )}
       </button>
     </form>
   );
